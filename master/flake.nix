bfbf39ef449279f804b3596ba7c141d0bb55110f
hugosenari <hugosenari@gmail.com>
2022-03-12 03:25:48 -0300

    chore: re index mime

---
+++ master/flake.nix
@@ -0,0 +1,26 @@
+{
+  description = ''Library for attaching files to emails.'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-mime-master.flake = false;
+  inputs.src-mime-master.owner = "enthus1ast";
+  inputs.src-mime-master.ref   = "refs/heads/master";
+  inputs.src-mime-master.repo  = "nimMime";
+  inputs.src-mime-master.type  = "github";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-mime-master"];
+  in lib.mkRefOutput {
+    inherit self nixpkgs ;
+    src  = deps."src-mime-master";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  };
+}
---
+++ v0_0_3/flake.nix
@@ -0,0 +1,26 @@
+{
+  description = ''Library for attaching files to emails.'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-mime-v0_0_3.flake = false;
+  inputs.src-mime-v0_0_3.owner = "enthus1ast";
+  inputs.src-mime-v0_0_3.ref   = "refs/tags/v0.0.3";
+  inputs.src-mime-v0_0_3.repo  = "nimMime";
+  inputs.src-mime-v0_0_3.type  = "github";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-mime-v0_0_3"];
+  in lib.mkRefOutput {
+    inherit self nixpkgs ;
+    src  = deps."src-mime-v0_0_3";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  };
+}
